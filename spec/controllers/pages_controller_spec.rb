f45ddc8bf9d321bf880ba098b32cead9bea403eb
William Jackson <william.jackson@utoronto.ca>
2011-11-24 14:50:37 -0500

    done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+        :content => "Victoria University Libraries Student Pay | Home")
+    end
+  end
+
+  describe "Get 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+        :content =>"Victoria University Libraries Student Pay | About")
     end
   end
 
