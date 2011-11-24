46cc3167ff244821f0cbc21511600ef1b0bedac7
William Jackson <william.jackson@utoronto.ca>
2011-11-24 14:09:00 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentPay::Application.config.secret_token = '5b8c447df2dc79e37105f62f0e72bf20d895118bfa97a18313abe73c651267e06f52515bceb5b266e60e4ebd133e7a3809ae71764476ff94e28f634cfc9c794e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StudentPay::Application.config.session_store :cookie_store, key: '_studentPay_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StudentPay::Application.config.session_store :active_record_store
