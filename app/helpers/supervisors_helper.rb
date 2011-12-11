407c5ad3977093739ae82116ef4c3d292b2b693d
William Jackson <william.jackson@utoronto.ca>
2011-12-11 11:39:38 -0500

    Added columns to supervisor view

--- app/helpers/supervisors_helper.rb
+++ app/helpers/supervisors_helper.rb
@@ -1,14 +1,27 @@
 module SupervisorsHelper
 
-  def render_multi_column_jobs(jobs, cols=2)
+  def render_multi_column_list(items, div)
     returning (rendered="") do
-      jobs.in_groups_of(jobs.size/cols) do |group|
-        rendered << %{ <div class='narrow-col'><ul> }
-        group.each do |job|
-          rendered << "<li> </li>" if job
-        end
-        rendered << %{ </ul></div> }
+      items_size = items.size
+      items_size = items_size + 1 if items_size % 2 == 1
+      items.in_groups_of(items_size / 2) do |group|
+        #group.each do |item|
+        # if item.instance_of?(PaySheet)
+        #  job = item.job
+        #else
+        # job = item
+        #end
+        #if job
+        rendered << "<p> hi </p>"
+        #rendered << "<%= link_to #{job.user.name}, #{job.user} %>"
+        # rendered << "<%=" "(#{job.name})" "%>"
+        # rendered << "<%= link_to" "view/update," "edit_pay_sheet_path(#{item}) %>" if div == "approved"
+        #rendered << "<%= link_to" "add pay sheet," "new_pay_sheet_path(:job_id => #{job}) %>" if div == "missing"
+        #   end
+        # end
+        #end
       end
     end
   end
 end
+
